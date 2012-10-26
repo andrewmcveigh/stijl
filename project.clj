2f773e8e47d3c373b9140e6fe9edddc9e7783908
Andrew Mcveigh <me@andrewmcveigh.com>
2012-10-26 20:34:45 +0200

    Removed plugins from project.clj.

--- project.clj
+++ project.clj
@@ -3,5 +3,4 @@
   :url "https://github.com/andrewmcveigh/stijl"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]]
-  :plugins [[lein-tarsier "0.9.2"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]])
